8554499e5ecedbda5004dc50b3d8b6cf59718081
Krasimir  Garev <kgarev@gmail.com>
2019-05-30 19:39:20 +0200

    for

---
+++ letter.py
@@ -0,0 +1,6 @@
+# ASCII TABLE convert str to chr from ascii
+# ord('a') and  ord('z') ot bukvi kam cifri
+# chr(letter)  ot cifri uam bukvi
+
+for letter in range(ord('a'), ord('z')):
+    print(chr(letter))
