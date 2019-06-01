600e0b85af88823cf67e1934edc7ea6485830e5b
Krasimir  Garev <kgarev@gmail.com>
2019-06-01 09:59:30 +0200

    for

---
+++ largest number.py
@@ -0,0 +1,10 @@
+n = int(input())
+max_number = int(input())   # -9223372036854775807
+
+for number in range(0, n):
+    input_number = int(input())
+    if input_number > max_number:
+        max_number = input_number
+
+
+print(max_number)
