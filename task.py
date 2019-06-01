97e7b1ce4256804788ba7e9db4a71b5bf97a62ce
Krasimir  Garev <kgarev@gmail.com>
2019-06-01 08:21:25 +0200

    for

--- task.py
+++ task.py
@@ -1,2 +1,9 @@
 for item in range(1, 21):
     print('item: ' + str(item))
+
+for number in range(1, 101):
+    print(number)
+
+for number in range(1, 1000):
+    if number % 10 == 7:
+        print(number)
