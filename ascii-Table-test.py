a9a317ea161a168a1d9c58c435381dbd05805d25
Krasimir  Garev <kgarev@gmail.com>
2019-06-01 09:24:26 +0200

    for

--- ascii-Table-test.py
+++ ascii-Table-test.py
@@ -1,2 +1,14 @@
 for latter in range(ord('a'), ord('z')+1):
     print(chr(latter))
+
+a = 'a'
+a = ord(a)
+print(chr(a))
+
+n = int(input())
+sum = 0
+
+for number in range(1, n+1):
+    input_number = int(input())
+    sum += input_number
+print(sum)
